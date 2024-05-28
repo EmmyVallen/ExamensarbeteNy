3f8f1c04879a1ad576fe16b339f1185ee7e6f688
Emmy Vallén <eemmyvallen@outlook.com>
2024-05-28 14:05:18 +0200

    Ta bort vara från kundkorg knapp implementerat. Har även lagt in alla produkter i kundkorg bara för att se så knappen fungerar. har ej fixat så man kan lägga in varor i kundkorg. knapp finns och man skickas till kundkorg MEN den upptaderas ej i databasen.

--- ExamensarbeteNy/Controllers/KundkorgController.cs
+++ ExamensarbeteNy/Controllers/KundkorgController.cs
@@ -14,8 +14,34 @@ namespace ExamensarbeteNy.Controllers
         }
         public IActionResult Index()
         {
-            ViewBag.AllCategories = _context.Kategorier.ToList();
+            // Hämta alla produkter i kundkorgen från databasen
+            var productsInCart = _context.Kundkorgar
+                .Include(k => k.Produkter)
+                .FirstOrDefault()?.Produkter;
+
+            return View(productsInCart);
+        }
+        [HttpPost]
+        public IActionResult LäggTill(int productId)
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult TaBort(int produktId)
+        {
+            // Hitta den specifika produkten i kundkorgen baserat på produktens ID
+            var productToRemove = _context.Produkter.Find(produktId);
+
+            if (productToRemove != null)
+            {
+                // Ta bort produkten från kundkorgen
+                _context.Produkter.Remove(productToRemove);
+                _context.SaveChanges();
+            }
+
+            // Omdirigera tillbaka till kundkorgen efter borttagning
+            return RedirectToAction("Index");
         }
 
     }
